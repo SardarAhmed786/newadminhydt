7c09c26e9c2261ecb84559df3a0cde7ff94699b9
m-jawa-d <chudhryjawad@gmail.com>
2024-07-12 17:03:50 +0500

    backend url changed

--- src/utils/environment.js
+++ src/utils/environment.js
@@ -20,7 +20,7 @@ let Environment = {
     "https://api.studio.thegraph.com/query/4177/hydt-sepolia/version/latest",
 
   // apiUrl: "http://ec2-18-236-118-29.us-west-2.compute.amazonaws.com:5555/",
-  apiUrl: "https://hydtapi.quecko.com/",
+  apiUrl: "https://hydtauditedapi.quecko.com/",
 };
 
 export default Environment;
